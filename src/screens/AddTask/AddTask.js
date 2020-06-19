5cc103668ec674f4fbcdef0cd837816ff559675a
AyrtonCossuol <ayrton.cossuol@live.com>
2020-06-19 11:32:10 -0300

    criado o modal de nova tarefa com estado mais ainda nao salvando

--- src/screens/AddTask/AddTask.js
+++ src/screens/AddTask/AddTask.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { View, Modal, TouchableWithoutFeedback } from 'react-native';
+import { View, Modal, Text, TouchableOpacity, TextInput, TouchableWithoutFeedback } from 'react-native';
 
 import styles from './styles';
 
+const initialState = { 
+    desc: '' ,
+};
+
 export default class AddTask extends Component {
+    state = {
+        ...initialState,
+    };
+
     render() {
         return(
             <Modal 
@@ -12,6 +20,31 @@ export default class AddTask extends Component {
                 onRequestClose={this.props.onCancel}
                 animationType='slide'
             >
+                <TouchableWithoutFeedback onPress={this.props.onCancel}>
+                    <View style={styles.background}>
+
+                    </View>
+                </TouchableWithoutFeedback>
+                
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                    <TextInput 
+                        style={styles.input}
+                        placeholder="Informa a Descrição..."
+                        onChangeText={desc => this.setState({ desc })}
+                        value={this.state.desc}
+                    />
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                
                 <TouchableWithoutFeedback onPress={this.props.onCancel}>
                     <View style={styles.background}>
 
--- src/screens/AddTask/styles.js
+++ src/screens/AddTask/styles.js
@@ -1,8 +1,44 @@
 import { StyleSheet} from 'react-native';
 
+import commonStyles from '../../commonStyles';
+
 export default StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+
+    container: {
+        backgroundColor: '#fff',
+    },
+
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 16,
+    },
+
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        margin: 15,
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#e3e3e3',
+        borderRadius: 6,
+    },
+
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today,
     }
 });
